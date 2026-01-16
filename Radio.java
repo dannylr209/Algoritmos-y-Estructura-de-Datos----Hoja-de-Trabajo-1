5382b9114d3976682e1501d31094da6197b6755f
Leo141592 <her25041@uvg.edu.gt>
2026-01-16 08:34:38 -0600

    Add files via upload

---
+++ Radio.java
@@ -0,0 +1,15 @@
+public interface Radio {
+    void prenderRadio();
+
+    void apagarRadio();
+
+    void avanzarEstacion();
+
+    void guardarEstacion(int numeroBoton);
+
+    void cargarEstacion(int numeroBoton);
+
+    void cambiarFM();
+
+    void cambiarAM();
+}
